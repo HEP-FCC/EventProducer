b6aafdc118ee4fc160c4176b8c96d164e9354a39
clementhelsens <clement.helsens@cern.ch>
2016-12-05 13:25:14 +0100

    modify param for delphes

--- param.py
+++ param.py
@@ -1,6 +1,10 @@
 indir ='/eos/fcc/hh/generation/mg5_amcatnlo/gridpacks/'
 outdir='/eos/fcc/hh/generation/mg5_amcatnlo/lhe/'
 
+version='v0_0'
+outdir_delphes='/eos/fcc/hh/generation/DelphesEvents/%s/'%version
+
+
 gridpacklist = [
 'pp_tt012j_5f',
 'pp_w012j_5f',
