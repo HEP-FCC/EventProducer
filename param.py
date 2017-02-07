4400d3104aa95ff2c2f918421703bcd02e5bdec2
clementhelsens <clement.helsens@cern.ch>
2017-02-07 22:22:28 +0100

    add filter effs

--- param.py
+++ param.py
@@ -6,9 +6,9 @@ outdir_delphes='/eos/fcc/hh/generation/DelphesEvents/%s/'%version
 
 
 gridpacklist = {
-'pp_tt012j_5f':['ttbar + 0,1,2 jets 5 flavor scheme','inclusive','xqcut = 60, qCut = 90', '4.2670e04',''],
-'pp_w012j_5f':['w + 0,1,2 jets 5 flavor scheme','inclusive','xqcut = 30, qCut = 45','1.4995e06', ''],
-'pp_z012j_5f':['z + 0,1,2 jets 5 flavor scheme','inclusive','xqcut = 30, qCut = 45', '5.1839e05', ''],
+'pp_tt012j_5f':['ttbar + 0,1,2 jets 5 flavor scheme','inclusive','xqcut = 60, qCut = 90', '4.2670e04','0.468'],
+'pp_w012j_5f':['w + 0,1,2 jets 5 flavor scheme','inclusive','xqcut = 30, qCut = 45','1.4995e06', '0.724'],
+'pp_z012j_5f':['z + 0,1,2 jets 5 flavor scheme','inclusive','xqcut = 30, qCut = 45', '5.1839e05', '0.691'],
 'pp_jjaa01j_5f':['dijet diphoton + 0,1,2 jets 5 flavor scheme','xqcut = 20, qCut = 30', '55.72','0.236'],
 'pp_jjaa_5f':['dijet diphoton','','17.97','1.0'],
 'pp_jjja01j_5f':['photon +jets + 0,1,2,3 jets 5 flavor scheme','xqcut = 20, qCut = 25','4.133e+05','0.143'],
