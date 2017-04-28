e99e44d347ea3e2537084f9d1cde2bda97e36742
clementhelsens <clement.helsens@cern.ch>
2017-04-28 13:18:01 +0200

    change cronrun directory

--- scripts/cronrun.sh
+++ scripts/cronrun.sh
@@ -1,11 +1,11 @@
 source /afs/cern.ch/user/h/helsens/eosfcc.sh
-python /afs/cern.ch/user/h/helsens/FCCsoft/Generators/LHEventProducer/jobchecker.py LHE
-python /afs/cern.ch/user/h/helsens/FCCsoft/Generators/LHEventProducer/cleanfailed.py LHE
+python /afs/cern.ch/user/h/helsens/FCCsoft/Generators/EventProducer/jobchecker.py LHE
+python /afs/cern.ch/user/h/helsens/FCCsoft/Generators/EventProducer/cleanfailed.py LHE
 
-python /afs/cern.ch/user/h/helsens/FCCsoft/Generators/LHEventProducer/jobchecker.py FCC
-python /afs/cern.ch/user/h/helsens/FCCsoft/Generators/LHEventProducer/cleanfailed.py FCC
+python /afs/cern.ch/user/h/helsens/FCCsoft/Generators/EventProducer/jobchecker.py FCC
+python /afs/cern.ch/user/h/helsens/FCCsoft/Generators/EventProducer/cleanfailed.py FCC
 
-python /afs/cern.ch/user/h/helsens/FCCsoft/Generators/LHEventProducer/printdicts.py /afs/cern.ch/work/h/helsens/public/FCCDicts/LHEdict.json /afs/cern.ch/user/h/helsens/www/LHEevents.txt
-python /afs/cern.ch/user/h/helsens/FCCsoft/Generators/LHEventProducer/printdicts.py /afs/cern.ch/work/h/helsens/public/FCCDicts/PythiaDelphesdict_v0_0.json /afs/cern.ch/user/h/helsens/www/Delphesevents.txt
+python /afs/cern.ch/user/h/helsens/FCCsoft/Generators/EventProducer/printdicts.py /afs/cern.ch/work/h/helsens/public/FCCDicts/LHEdict.json /afs/cern.ch/user/h/helsens/www/LHEevents.txt
+python /afs/cern.ch/user/h/helsens/FCCsoft/Generators/EventProducer/printdicts.py /afs/cern.ch/work/h/helsens/public/FCCDicts/PythiaDelphesdict_v0_0.json /afs/cern.ch/user/h/helsens/www/Delphesevents.txt
 
-python /afs/cern.ch/user/h/helsens/FCCsoft/Generators/LHEventProducer/makeSampleList.py  /afs/cern.ch/work/h/helsens/public/FCCDicts/PythiaDelphesdict_v0_0.json
+python /afs/cern.ch/user/h/helsens/FCCsoft/Generators/EventProducer/makeSampleList.py  /afs/cern.ch/work/h/helsens/public/FCCDicts/PythiaDelphesdict_v0_0.json
