6219dbdbd0e65fb5ac1074e250c5c7fb5cd78753
clementhelsens <clement.helsens@cern.ch>
2017-05-05 13:02:57 +0200

    add python and root in init

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_87/Python/2.7.10/x86_64-slc6-gcc49-opt/Python-env.sh
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_87/ROOT/6.08.02/x86_64-slc6-gcc49-opt/ROOT-env.sh
 export EOS_MGM_URL="root://eospublic.cern.ch"
 source /afs/cern.ch/project/eos/installation/client/etc/setup.sh
 export EVENTPRODUCER=$PWD
