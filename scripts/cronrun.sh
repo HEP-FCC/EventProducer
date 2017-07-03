6b451e94c59c9c8d3e39b85036fa4caf45af157e
clementhelsens <clement.helsens@cern.ch>
2017-07-03 13:39:06 +0200

    add version in cronrun

--- scripts/cronrun.sh
+++ scripts/cronrun.sh
@@ -7,10 +7,16 @@ source ./init.sh
 python common/jobchecker.py LHE
 python common/cleanfailed.py LHE
 
-python common/jobchecker.py FCC
-python common/cleanfailed.py FCC
+python common/jobchecker.py FCC_fcc_v01
+python common/cleanfailed.py FCC_fcc_v01
+
+python common/jobchecker.py FCC_cms
+python common/cleanfailed.py FCC_cms
 
 python common/printdicts.py LHE /afs/cern.ch/user/h/helsens/www/LHEevents.txt
-python common/printdicts.py FCC /afs/cern.ch/user/h/helsens/www/Delphesevents.txt
+python common/printdicts.py FCC_fcc_v01 /afs/cern.ch/user/h/helsens/www/Delphesevents_fcc_v01.txt
+python common/printdicts.py FCC_cms /afs/cern.ch/user/h/helsens/www/Delphesevents_cms.txt
 
-python common/makeSampleList.py
+python common/makeSampleList.py fcc_v01
+python common/makeSampleList.py cms
+
