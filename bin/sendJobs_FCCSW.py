9e556ef8e543673b29b7d96383c3a33b71f341bd
clementhelsens <clement.helsens@cern.ch>
2017-07-12 15:35:32 +0200

    bug fix for decays

--- bin/sendJobs_FCCSW.py
+++ bin/sendJobs_FCCSW.py
@@ -211,7 +211,7 @@ if __name__=="__main__":
         njobstmp=njobs
         ################# continue if job already exist and process if not
         while i<njobstmp:
-            if outdict.jobexits(sample=pr,jobid=i): 
+            if outdict.jobexits(sample=pr_decay,jobid=i): 
                 print 'job i ',i,'  exists    njobs ',njobs
                 i+=1
                 njobstmp+=1
