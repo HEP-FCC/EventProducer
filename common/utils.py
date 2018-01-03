2a5fddd09a2770238e44df9c645010b4b37a57a5
clementhelsens <clement.helsens@cern.ch>
2018-01-03 14:31:47 +0100

    add stuff in utils

--- common/utils.py
+++ common/utils.py
@@ -1,5 +1,9 @@
 import warnings
 import ROOT as r
+import subprocess
+import os
+from pwd import getpwuid
+import sys
 # adding this because heppy does not handle root recovered trees'
 #_____________________________________________________________
 def isValidROOTfile(infile):
@@ -12,4 +16,21 @@ def isValidROOTfile(infile):
                 valid = False
     return valid
 
-#________________________________________________________________
+
+#__________________________________________________________
+def getCommandOutput(command):
+    p = subprocess.Popen(command, shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    stdout,stderr = p.communicate()
+    return {"stdout":stdout, "stderr":stderr, "returncode":p.returncode}
+
+#__________________________________________________________
+def testeos(f,fs):
+    exist=os.path.isfile(f)
+    if exist:
+        size = os.path.getsize(f)
+        if size==fs:
+            return True
+    return False
+#__________________________________________________________
+def find_owner(filename):
+    return getpwuid(os.stat(filename).st_uid).pw_name
