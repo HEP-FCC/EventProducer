bd47b9e2c98b2d8b4ff1169088b7d310691a5fc8
clementhelsens <clement.helsens@cern.ch>
2018-01-03 14:31:23 +0100

    remove root file is valid

--- common/jobchecker.py
+++ common/jobchecker.py
@@ -8,22 +8,7 @@ import os.path
 import ROOT as r
 import os.path
 import EventProducer.common.isreading as isr
-import warnings
 import EventProducer.common.utils as ut
-import warnings
-
-# adding this because heppy does not handle root recovered trees'
-#_____________________________________________________________
-def isValidROOTfile(infile):
-    valid = True
-    with warnings.catch_warnings(record=True) as was:
-        f=r.TFile.Open(infile)
-        ctrlstr = 'probably not closed'
-        for w in was:
-            if ctrlstr in str(w.message):
-                valid = False
-    return valid
-#________________________________________________________________
 
 
 if "secret" in sys.argv:
