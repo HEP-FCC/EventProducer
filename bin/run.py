f3042b651a0341747cb944076e1cb8d7423e9151
clementhelsens <clement.helsens@cern.ch>
2018-01-29 16:53:42 +0100

    new classes

--- bin/run.py
+++ bin/run.py
@@ -1,26 +1,20 @@
 #python bin/run.py --HELHC --LHE --send -p mg_pp_ee_lo -n 10000 -N 100 --lsf -q 1nh
 #python bin/run.py --HELHC --LHE --check --dir /eos/experiment/fcc/helhc/generation/lhe/
+#python bin/run.py --HELHC --LHE --merge
 #python bin/run.py --HELHC --LHE --web
-#python bin/run.py --HELHC --LHE --clean -p mg_pp_ee_lo
 #python bin/run.py --HELHC --LHE --remove -p mg_pp_ee_lo
+#python bin/run.py --HELHC --LHE --clean -p mg_pp_ee_lo
 
 #python bin/run.py --HELHC --reco --send -p mg_pp_ee_lo --type lhep8 -N 1 --lsf -q 1nh --version helhc_v01
 #python bin/run.py --HELHC --reco --send -p pp_Zprime_10TeV_ll -n 10000 --type p8 -N 1 --lsf -q 1nh --version helhc_v01
 #python bin/run.py --HELHC --reco --check --version helhc_v01
-#python bin/run.py --HELHC --reco --web
-
+#python bin/run.py --HELHC --reco --merge --version helhc_v01
+#python bin/run.py --HELHC --reco --web --version helhc_v01
+#python bin/run.py --HELHC --reco --remove -p mgp8_pp_ee_lo
+#python bin/run.py --HELHC --reco --clean --version helhc_v01 -p p8_pp_Zprime_10TeV_ttbar
 
 import sys
-
 import EventProducer.common.utils as ut
-import EventProducer.common.checker as chk
-import EventProducer.common.printdicts as pdic
-import EventProducer.common.cleanfailed as clf
-import EventProducer.common.removeProcess as rmp
-
-import EventProducer.bin.send_lhe as slhe
-import EventProducer.bin.send_p8 as sp8
-import EventProducer.bin.send_lhep8 as slhep8
 
 #__________________________________________________________
 if __name__=="__main__":
@@ -40,6 +34,7 @@ if __name__=="__main__":
 
     jobTypeGroup = parser.add_mutually_exclusive_group(required = True) # Type of events to generate
     jobTypeGroup.add_argument("--check", action='store_true', help="run the jobchecker")
+    jobTypeGroup.add_argument("--merge", action='store_true', help="merge the yaml for all the processes")
     jobTypeGroup.add_argument("--send", action='store_true', help="send the jobs")
     jobTypeGroup.add_argument("--clean", action='store_true', help="clean the dictionnary and eos from bad jobs")
     jobTypeGroup.add_argument("--web", action='store_true', help="print the dictionnary for webpage")
@@ -65,7 +60,7 @@ if __name__=="__main__":
     
 
     if args.FCC:
-        import EventProducer.config.param as para
+        import EventProducer.config.param_FCC as para
         print 'import FCC config'
     elif args.HELHC:
         import EventProducer.config.param_HELHC as para
@@ -77,29 +72,28 @@ if __name__=="__main__":
 
     versionGroup = parser.add_argument_group('recontruction version')
     versionGroup.add_argument('--version', type=str, required = '--reco' in sys.argv, help='Version to use', choices = para.fcc_versions)
-    decaylist=[d for d in para.decaylist]
-    print decaylist
+    decaylist=[]
+    for key, value in para.decaylist.iteritems():
+        for v in value:
+            if v  not in decaylist: decaylist.append(v)
+        
     sendjobGroup.add_argument('-d', '--decay', type=str, default='', help='pythia8 decay when needed', choices=decaylist)
 
     args, _ = parser.parse_known_args()
     version = args.version
 
-    indict=None
-    inread=None
     indir=None
+    yamldir=None
     fext=None
 
     if args.LHE:
-        inread=para.readlhe_dic
-        indict=para.lhe_dic
         indir=para.lhe_dir
         fext=para.lhe_ext
-
+        yamldir=para.yamldir+'lhe'
     elif args.reco:
-        indict=para.fcc_dic.replace('VERSION',version)
-        inread=para.readfcc_dic.replace('VERSION',version)
         indir='%s%s'%(para.delphes_dir,version)
         fext=para.delphes_ext
+        yamldir=para.yamldir+version
         print 'Running reco production system with version %s'%version
     else:
         print 'problem, need to specify --reco or --LHE'
@@ -117,10 +111,17 @@ if __name__=="__main__":
             indir=args.dir
         if args.process!='':
             print 'using a specific process ',args.process
-        checker=chk.checker(indict, indir, inread, para, fext, args.process)
+        import EventProducer.common.checker_yaml as chky
+        checker=chky.checker_yaml(indir, para, fext, args.process, version)
         checker.check()
 
     
+    elif args.merge:
+        print 'running the merger'
+        import EventProducer.common.merger as mgr
+        merger = mgr.merger(para.yamldir)
+        merger.merge()
+
     elif args.send:
         print 'sending jobs'        
         if args.lsf:
@@ -132,27 +133,34 @@ if __name__=="__main__":
  
         if args.LHE:
             print 'preparing to send lhe jobs from madgraph gridpacks for process {}'.format(args.process)
+            import EventProducer.bin.send_lhe as slhe
             sendlhe=slhe.send_lhe(args.numJobs,args.numEvents, args.process, args.lsf, args.queue, para)
             sendlhe.send()
         elif args.reco:
             if sendOpt=='lhep8':
                 print 'preparing to send FCCSW jobs from lhe'
+                import EventProducer.bin.send_lhep8 as slhep8
                 sendlhep8=slhep8.send_lhep8(args.numJobs,args.numEvents, args.process, args.lsf, args.queue, para, version, args.decay)
                 sendlhep8.send()
             elif sendOpt=='p8':
                 print 'preparing to send FCCSW jobs from pythia8 directly'
+                import EventProducer.bin.send_p8 as sp8
                 sendp8=sp8.send_p8(args.numJobs,args.numEvents, args.process, args.lsf, args.queue, para, version)
                 sendp8.send()
 
     elif args.web:
         if args.LHE: 
             print 'create web page for LHE'         
-            printdic=pdic.printdicts(indict, para.lhe_web, False, True, para)
+            import EventProducer.common.printer as prt
+            printdic=prt.printer(yamldir,para.lhe_web, False, True, para)
             printdic.run()
+
+
         elif args.reco:
             print 'create web page for reco version %s'%version
             webpage=para.delphes_web.replace('VERSION',version)
-            printdic=pdic.printdicts(indict, webpage, True, False, para, version)
+            import EventProducer.common.printer as prt
+            printdic=prt.printer(yamldir, webpage, True, False, para, version)
             printdic.run()
 
     elif args.remove:
@@ -160,14 +168,18 @@ if __name__=="__main__":
             print 'need to specify a process, exit'
             sys.exit(3)
         if args.LHE: 
-            print 'remove process %s from the dictionnary and eos for LHE'%args.process
-            removeProcess = rmp.removeProcess(indict, inread, args.process, indir)
-            removeProcess.remove()
+            print 'remove process %s from eos and database for LHE'%args.process
+        elif args.reco: 
+            print 'remove process %s from eos and database for reco'%args.process
+        import EventProducer.common.removeProcess as rmp
+        removeProcess = rmp.removeProcess(args.process, indir, yamldir)
+        removeProcess.remove()
         
 
     elif args.clean:
         print 'clean the dictionnary and eos'
-        clean=clf.cleanfailed(indict, inread, args.process)
+        import EventProducer.common.cleanfailed as clf
+        clean=clf.cleanfailed(indir, yamldir, args.process)
         clean.clean()
     else:
         print 'problem, need to specify --check or --send'
