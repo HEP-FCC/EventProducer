4180da7ee136f6fb8e5005023f62dd3ad83ab3fb
clementhelsens <clement.helsens@cern.ch>
2018-01-29 10:06:54 +0100

    add FCC setup to init

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_87/Python/2.7.10/x86_64-slc6-gcc49-opt/Python-env.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_87/ROOT/6.08.02/x86_64-slc6-gcc49-opt/ROOT-env.sh
+source /cvmfs/fcc.cern.ch/sw/0.8.2/init_fcc_stack.sh
 export EOS_MGM_URL="root://eospublic.cern.ch"
 export EVENTPRODUCER=$PWD
 export PYTHONPATH=$PWD/..:$PYTHONPATH
