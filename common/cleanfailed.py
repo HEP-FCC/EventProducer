dda285e0771a73d325ffe999ca05acd1bd8f8690
clementhelsens <clement.helsens@cern.ch>
2018-01-29 16:56:12 +0100

    cleanfailed w/o dic

--- common/cleanfailed.py
+++ common/cleanfailed.py
@@ -2,52 +2,53 @@
 #python cleanfailed.py LHE/FCC_fcc_v01 secret
 #python cleanfailed.py LHE/FCC_cms secret
 
-import json
+import yaml
 import os
+import glob
 import os.path
 import sys
-import EventProducer.common.isreading as isr
 
 
 class cleanfailed():
 
 #__________________________________________________________
-    def __init__(self, indict, inread, process):
-        self.indict = indict
-        self.inread = inread
+    def __init__(self, indir, yamldir, process):
+        self.indir = indir+'/'+process
+        self.yamldir = yamldir+'/'+process
         self.process = process
-        if os.path.isfile(self.indict)==False:
-            print 'dictonary does not exists '
-            sys.exit(3)
-
-
-        self.readdic=isr.isreading(self.inread, self.indict)
-        self.readdic.backup('cleanfailed')
-        self.readdic.reading()
-
 #__________________________________________________________
     def clean(self):
         nfailed=0
-        mydict=None
-        mynewdict={}
-        user=os.environ['USER']
-        with open(self.indict) as f:
-            mydict = json.load(f)
-        for s in mydict:
-            mynewdict[s]=[]
-            if self.process != '' and self.process != s: 
-                mynewdict[s]=mydict[s]
+        ldir=[x[0] for x in os.walk(self.yamldir)]
+        print ldir
+       
+        for l in ldir:
+      
+            All_files = glob.glob("%s/events_*.yaml"%l)
+            if len(All_files)==0:continue
+            keys=l.split('/')
+            if keys[-1]!='':process=keys[-1]
+            else:process=keys[len(keys)-2]            
+            print 'process from the input directory ',process
+            if self.process!='' and self.process!=process: 
                 continue
-            for j in xrange(len(mydict[s])):
-                if mydict[s][j]['status']=='BAD'and user in (mydict[s][j]['user']): 
-                    print 'job failed ',j,'  ',mydict[s][j]['out']
-                    nfailed+=1
-                    os.system('rm %s'%(mydict[s][j]['out']))
-            
-                else: mynewdict[s].append(mydict[s][j])
-        
-        with open(self.indict, 'w') as f:
-            json.dump(mynewdict, f)
 
-        self.readdic.comparedics(nf=nfailed, ns=0)
-        self.readdic.finalize()
+            for f in All_files:
+                if not os.path.isfile(f): 
+                    print 'file does not exists... %s'%f
+                    continue
+                
+                with open(f, 'r') as stream:
+                    try:
+                       tmpf = yaml.load(stream)
+                       if tmpf['processing']['status']=='BAD':
+                           nfailed+=1
+                           cmd="rm %s"%(tmpf['processing']['out'])
+                           print cmd
+                           cmd="rm %s"%(f)
+                           print cmd
+                           #os.system(cmd)
+                    except yaml.YAMLError as exc:
+                        print(exc)
+
+   
