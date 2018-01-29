af5996dfee042aa32bef76867bd3eb63689cadbb
clementhelsens <clement.helsens@cern.ch>
2018-01-29 16:57:51 +0100

    new removeprocess

--- common/removeProcess.py
+++ common/removeProcess.py
@@ -1,53 +1,33 @@
 #python jobchecker.py LHE or FCC
-import json
-import subprocess
-import sys
-import os.path
-
-import EventProducer.config.param as para
-import EventProducer.common.isreading as isr
+import EventProducer.common.utils as ut
+import os, sys
 
 class removeProcess():
 
-
 #__________________________________________________________
-    def __init__(self, indict, inread, process, indir):
-        self.indict = indict
-        self.inread = inread
+    def __init__(self, process, indir, yamldir):
         self.process = process
         self.indir = indir
-        if os.path.isfile(self.indict)==False:
-            print 'dictonary does not exists '
-            sys.exit(3)
+        self.yamldir = yamldir
 
-        self.readdic=isr.isreading(self.inread, self.indict)
-        self.readdic.backup('removeProcess')
-        self.readdic.reading()
+        if not ut.dir_exist(indir+'/'+process):
+            print 'process eos does not exist, exit ',indir+'/'+process
+            sys.exit(3)
+        if not ut.dir_exist(yamldir+'/'+process):
+            print 'process yaml does not exist, exit ',yamldir+'/'+process
+            sys.exit(3)
 
 
 #__________________________________________________________
     def remove(self):
-        mydict=None
-        with open(self.indict) as f:
-            mydict = json.load(f)
-
-        mynewdict={}
-        for element in mydict:
-            if self.process == str(element):
-                print self.process,'    ',str(element)
-                continue
-            else:
-                mynewdict[element]=mydict[element]
-
-        with open(self.indict, 'w') as f:
-            json.dump(mynewdict, f)
-
-        self.readdic.comparedics(nf=0,ns=1)
-        self.readdic.finalize()
-
-
-        import os
+        
+        
         print 'remove process in eos'
         cmd="rm %s/%s/events*"%(self.indir, self.process)
         print cmd
         os.system(cmd)
+        print 'remove process in yaml'
+        cmd="rm %s/%s/events*"%(self.yamldir, self.process)
+        print cmd
+        os.system(cmd)
+ 
