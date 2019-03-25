b9dafabb1252b065e79e34b96780f4a9e50988ec
David Olivier Jamin <david.jamin@cern.ch>
2019-03-25 13:41:55 +0100

    minor update in condor str

--- bin/send_lhep8.py
+++ bin/send_lhep8.py
@@ -242,9 +242,9 @@ class send_lhep8():
             frun_condor.write('environment    = "LS_SUBCWD=%s"\n'%logdir) # not sure
             frun_condor.write('request_memory = 4G\n')
             # tmp
-            frun_condor.write('requirements   = ( (OpSysAndVer =?= "SLCern6") && (CERNEnvironment =?= "qa") &&  (Machine =!= LastRemoteHost) )\n')
+            frun_condor.write('requirements   = ( (OpSysAndVer =?= "SLCern6") && (CERNEnvironment =?= "qa") && (Machine =!= LastRemoteHost) )\n')
             # final to use when nodes have the fix
-            #frun_condor.write('requirements   = ( (OpSysAndVer =?= "SLCern6") &&  (Machine =!= LastRemoteHost) )\n')
+            #frun_condor.write('requirements   = ( (OpSysAndVer =?= "SLCern6") && (Machine =!= LastRemoteHost) )\n')
             frun_condor.write('on_exit_remove = (ExitBySignal == False) && (ExitCode == 0)\n')
             frun_condor.write('max_retries    = 3\n')
             frun_condor.write('+JobFlavour    = "%s"\n'%self.queue)
