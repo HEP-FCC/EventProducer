35da8f37119cbdaaa73660a41c5a6aff6db21a51
clementhelsens <clement.helsens@cern.ch>
2019-08-15 18:11:35 +0200

    modified init to take the latest FCCSW

--- init.sh
+++ init.sh
@@ -1,7 +1,4 @@
-#source /cvmfs/fcc.cern.ch/sw/0.8.2/init_fcc_stack.sh
-source /cvmfs/fcc.cern.ch/sw/views/releases/externals/94.0.0/x86_64-slc6-gcc62-opt/setup.sh
-
-#source /cvmfs/fcc.cern.ch/testing/sw/views/stable/x86_64-slc6-gcc62-opt/setup.sh
+source /cvmfs/fcc.cern.ch/sw/latest/setup.sh
 export EOS_MGM_URL="root://eospublic.cern.ch"
 export EVENTPRODUCER=$PWD
 export PYTHONPATH=$PWD/..:$PYTHONPATH
