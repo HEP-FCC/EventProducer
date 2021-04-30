a9bc57528043d9c2faa80960841464748319cc08
clementhelsens <clement.helsens@cern.ch>
2021-04-30 17:22:12 +0200

    change the stack

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-source /cvmfs/fcc.cern.ch/sw/latest/setup.sh
+source /cvmfs/sw.hsf.org/spackages2/key4hep-stack/2021-04-30/x86_64-centos7-gcc8.3.0-opt/t5gcd6ltt2ikybap2ndoztsg5uyorxzg/setup.sh
 export EOS_MGM_URL="root://eospublic.cern.ch"
 export EVENTPRODUCER=$PWD
 export PYTHONPATH=$PWD/..:$PYTHONPATH
