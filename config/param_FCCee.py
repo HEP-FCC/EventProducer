d02f37f1639eb6334ab7391df508ad324bff9c07
Perez <Emmanuel.Perez@cern.ch>
2021-07-11 10:19:39 +0200

    Fix typo for wzp6_ee_ee_Mee_30_150_ecm240

--- config/param_FCCee.py
+++ config/param_FCCee.py
@@ -306,7 +306,7 @@ gridpacklist = {
    
     'wzp6_ee_mumu_ecm240':['Z/gamma* to mumu, ecm=240 GeV','full phase space','','5.288','1.0','1.0'],
     'wzp6_ee_tautau_ecm240':['Z/gamma* to tautau, ecm=240 GeV','full phase space','','4.668','1.0','1.0'],
-    'wzp6_ee_ee_Mee_30_150_ecm240':['ee (s and t), ecm=240 GeV','30 < Mee < 150 GeV, 15 < theta < 165 deg','8.305','1.0','1.0'],
+    'wzp6_ee_ee_Mee_30_150_ecm240':['ee (s and t), ecm=240 GeV','30 < Mee < 150 GeV, 15 < theta < 165 deg','','8.305','1.0','1.0'],
 
 
 
