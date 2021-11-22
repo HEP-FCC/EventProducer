5f969e7cd043f5f3bb083dd015b27588d6498604
Perez <Emmanuel.Perez@cern.ch>
2021-11-22 18:47:14 +0100

    Added p8_ee_Zqq_ecm365 to param_FCCee.py

--- config/param_FCCee.py
+++ config/param_FCCee.py
@@ -94,6 +94,7 @@ pythialist={
     'p8_ee_tt_ecm365':['tt ecm=365GeV','inclusive decays','','0.800','1.0','1.0'],
 
     'p8_ee_WW_mumu_ecm365':['WW ecm=365GeV','W->mu and W ->tau->mu','','0.1785','1.0','1.0'],
+    'p8_ee_Zqq_ecm365':['Z/Gamma* ecm=365GeV','hadronic decays','','21.4149','1.0','1.0'],  
 
     'p8_ee_tt_fullhad_ecm365':['tt ecm=365GeV','hadronic decays','','0.363528','1.0','1.0'],
     'p8_ee_ZZ_fullhad_ecm365':['ZZ ecm=365GeV','hadronic decays','','0.31417','1.0','1.0'],
