7444a724a6f94998e13a2d20222e378e8ad76c3e
Juraj Smiesko <juraj.smiesko@cern.ch>
2023-05-08 18:11:17 +0200

    parameter instead of spring2021

--- scripts/cronrun_STDHEP_FCCee.sh
+++ scripts/cronrun_STDHEP_FCCee.sh
@@ -1,11 +1,11 @@
 cd /afs/cern.ch/user/f/fccsw/private/EventProducer/
 source ./init.sh
 
-python bin/run.py --FCCee --STDHEP --checkeos --prodtag spring2021
-python bin/run.py --FCCee --STDHEP --check --prodtag spring2021
-python bin/run.py --FCCee --STDHEP --merge --prodtag spring2021
-python bin/run.py --FCCee --STDHEP --clean --prodtag spring2021
-python bin/run.py --FCCee --STDHEP --cleanold --prodtag spring2021
-python bin/run.py --FCCee --STDHEP --merge --prodtag spring2021
-python bin/run.py --FCCee --STDHEP --web --prodtag spring2021
+python bin/run.py --FCCee --STDHEP --checkeos --prodtag ${1}
+python bin/run.py --FCCee --STDHEP --check --prodtag ${1}
+python bin/run.py --FCCee --STDHEP --merge --prodtag ${1}
+python bin/run.py --FCCee --STDHEP --clean --prodtag ${1}
+python bin/run.py --FCCee --STDHEP --cleanold --prodtag ${1}
+python bin/run.py --FCCee --STDHEP --merge --prodtag ${1}
+python bin/run.py --FCCee --STDHEP --web --prodtag ${1}
 
