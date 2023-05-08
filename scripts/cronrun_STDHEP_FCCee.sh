b84c17c6e9c46727b57d129df26b739070722c6b
Juraj Smiesko <juraj.smiesko@cern.ch>
2023-05-08 18:27:29 +0200

    Adding timestamp for stdhep fccee

--- scripts/cronrun_STDHEP_FCCee.sh
+++ scripts/cronrun_STDHEP_FCCee.sh
@@ -1,11 +1,13 @@
 cd /afs/cern.ch/user/f/fccsw/private/EventProducer/
 source ./init.sh
 
-python bin/run.py --FCCee --STDHEP --checkeos --prodtag ${1}
-python bin/run.py --FCCee --STDHEP --check --prodtag ${1}
-python bin/run.py --FCCee --STDHEP --merge --prodtag ${1}
-python bin/run.py --FCCee --STDHEP --clean --prodtag ${1}
-python bin/run.py --FCCee --STDHEP --cleanold --prodtag ${1}
-python bin/run.py --FCCee --STDHEP --merge --prodtag ${1}
-python bin/run.py --FCCee --STDHEP --web --prodtag ${1}
+python bin/run.py --FCCee --STDHEP --checkeos --prodtag ${1} > /dev/null
+python bin/run.py --FCCee --STDHEP --check --prodtag ${1} > /dev/null
+python bin/run.py --FCCee --STDHEP --merge --prodtag ${1} > /dev/null
+python bin/run.py --FCCee --STDHEP --clean --prodtag ${1} > /dev/null
+python bin/run.py --FCCee --STDHEP --cleanold --prodtag ${1} > /dev/null
+python bin/run.py --FCCee --STDHEP --merge --prodtag ${1} > /dev/null
+python bin/run.py --FCCee --STDHEP --web --prodtag ${1} > /dev/null
 
+mkdir -p ${EVENTPRODUCER}/log
+echo "Last run finished: `date`" > ${EVENTPRODUCER}/log/cronrun_STDHEP_FCCee.log
