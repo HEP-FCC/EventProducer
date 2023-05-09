fb96620f9e528740f4cff113cae189347484c401
Juraj Smiesko <juraj.smiesko@cern.ch>
2023-05-09 10:25:46 +0200

    Implementing timestamps for all cronjobs

--- scripts/cronrun_LHE_FCCee.sh
+++ scripts/cronrun_LHE_FCCee.sh
@@ -1,11 +1,13 @@
 cd /afs/cern.ch/user/f/fccsw/private/EventProducer/
 source ./init.sh
 
-python bin/run.py --FCCee --LHE --checkeos
-python bin/run.py --FCCee --LHE --check
-python bin/run.py --FCCee --LHE --merge
-python bin/run.py --FCCee --LHE --clean
-python bin/run.py --FCCee --LHE --cleanold
-python bin/run.py --FCCee --LHE --merge
-python bin/run.py --FCCee --LHE --web
+python bin/run.py --FCCee --LHE --checkeos > /dev/null
+python bin/run.py --FCCee --LHE --check > /dev/null
+python bin/run.py --FCCee --LHE --merge > /dev/null
+python bin/run.py --FCCee --LHE --clean > /dev/null
+python bin/run.py --FCCee --LHE --cleanold > /dev/null
+python bin/run.py --FCCee --LHE --merge > /dev/null
+python bin/run.py --FCCee --LHE --web > /dev/null
 
+mkdir -p ${EVENTPRODUCER}/log
+echo "Last run finished: `date`" > ${EVENTPRODUCER}/log/cronrun_LHE_FCCee.log
