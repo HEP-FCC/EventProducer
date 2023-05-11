17bb87b09662e00054aa82774711b10acf73caca
Birgit Stapf <bistapf@desy.de>
2023-05-11 17:38:40 +0200

    update edm4hep output config for FCC-hh scenarios study, include objects before isolation and OR

--- edm4hep_output_config.tcl
+++ edm4hep_output_config.tcl
@@ -1,11 +1,11 @@
 
 module EDM4HepOutput EDM4HepOutput {
-    add ReconstructedParticleCollections EFlowTrack EFlowPhoton EFlowNeutralHadron
+    add ReconstructedParticleCollections EFlowTrack EFlowPhoton EFlowNeutralHadron 
     add GenParticleCollections           Particle
-    add JetCollections                   Jet
-    add MuonCollections                  Muon MuonNoIso MuonNoOR
-    add ElectronCollections              Electron ElectronNoIso ElectronNoOR
-    add PhotonCollections                Photon PhotonNoIso PhotonNoOR
+    add JetCollections                   Jet JetNoIso
+    add MuonCollections                  Muon MuonNoIso 
+    add ElectronCollections              Electron ElectronNoIso 
+    add PhotonCollections                Photon PhotonNoIso 
     add MissingETCollections             MissingET
     add ScalarHTCollections              ScalarHT
     set RecoParticleCollectionName       ReconstructedParticles
