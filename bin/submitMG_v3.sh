5c07005f48c74a6072c90852c3edfe88941cd77c
Michele Selvaggi <michele.selvaggi@uclouvain.be>
2025-04-27 18:39:43 +0200

    fixed madspin jobs

--- bin/submitMG_v3.sh
+++ bin/submitMG_v3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#unset LD_LIBRARY_PATH
-#unset PYTHONHOME
-#unset PYTHONPATH
+unset LD_LIBRARY_PATH
+unset PYTHONHOME
+unset PYTHONPATH
 
 # source /cvmfs/sft.cern.ch/lcg/views/LCG_89/x86_64-centos7-gcc7-opt/setup.sh 
 # export LHAPDF_DATA_PATH=/afs/cern.ch/work/b/bistapf/lhapdf/LHAPDF-6.1.6/install/share/LHAPDF/
@@ -18,6 +18,23 @@ MODELFILE=${7}
 
 mkdir job
 cd job
+
+source /cvmfs/sft.cern.ch/lcg/views/LCG_107/x86_64-el9-gcc14-opt/setup.sh;
+
+# Create a local fake f2py3.11 pointing to f2py
+mkdir -p $PWD/f2py_bin
+ln -sf $(dirname $(which python3))/f2py $PWD/f2py_bin/f2py3.11
+
+# Prepend it to PATH
+export PATH=$PWD/f2py_bin:$PATH
+
+echo "python3 location: $(which python3)"
+echo "python3 --version: $(python3 --version)"
+echo "numpy version: $(python3 -c 'import numpy; print(numpy.__version__)')"
+echo "f2py3.11 location: $(which f2py3.11)"
+head -5 $(which f2py3.11)
+
+
 
 echo "copying madgraph tarball ..."
 # cp  /eos/experiment/fcc/hh/utils/generators/MG5_aMC_v2.5.5.tar.gz .
@@ -79,14 +96,38 @@ OUTDIR=${OUTPUTDIR}/${PROCESSNAME}
 OUTFILE=${OUTDIR}/events_${JOBID}.lhe.gz
 
 echo "Copying LHE file to ${OUTFILE}"
+echo "OUTDIR = ${OUTDIR}"
 mkdir -p ${OUTDIR}
+
+echo "Current working directory = ${PWD}"
 
 # check if this run is decayed or not
 if [ -d "${PWD}/DUMMYPROCESS/Events/run_01_decayed_1" ]; then
-  INFILE="${PWD}/DUMMYPROCESS/Events/run_01/unweighted_events.lhe.gz"
-else
+  echo "Found directory: ${PWD}/DUMMYPROCESS/Events/run_01_decayed_1"
+  echo "Using MadSpin decayed events"
   INFILE="${PWD}/DUMMYPROCESS/Events/run_01_decayed_1/unweighted_events.lhe.gz"
+else
+  echo "Did NOT find directory: ${PWD}/DUMMYPROCESS/Events/run_01_decayed_1"
+  echo "Using standard events"
+  INFILE="${PWD}/DUMMYPROCESS/Events/run_01/unweighted_events.lhe.gz"
 fi
 
-#source /cvmfs/sft.cern.ch/lcg/views/LCG_96/x86_64-centos7-gcc8-opt/setup.sh
+echo "Input file path = ${INFILE}"
+
+if [ -f "${INFILE}" ]; then
+  echo "Input file exists, ready to copy."
+else
+  echo "ERROR: Input file does not exist!"
+  echo "Listing Events directory for debug:"
+  ls -lh ${PWD}/DUMMYPROCESS/Events/
+  echo "Listing inside run_01_decayed_1 if exists:"
+  ls -lh ${PWD}/DUMMYPROCESS/Events/run_01_decayed_1 || echo "run_01_decayed_1 not found"
+  echo "Listing inside run_01:"
+  ls -lh ${PWD}/DUMMYPROCESS/Events/run_01 || echo "run_01 not found"
+  exit 1
+fi
+
+# Copy file
+echo "Running xrdcp command:"
+echo "xrdcp -N -v ${INFILE} root://eospublic.cern.ch/${OUTDIR}/events_${JOBID}.lhe.gz"
 xrdcp -N -v ${INFILE} root://eospublic.cern.ch/${OUTDIR}/events_${JOBID}.lhe.gz
