0244fe5a84d6902b0564d4ebf417549186dcc10b
Louis Portales <louis.portales@cern.ch>
2025-07-09 16:17:49 +0200

    updated IDEA config in param_FCCee.py

--- config/param_FCCee.py
+++ config/param_FCCee.py
@@ -79,6 +79,7 @@ kkmccards_dir = eosbaseinputdir+"FCC-config/_VERSION_/FCCee/Generator/KKMC/"
 #detectors = ['IDEA', 'IDEA_3T', 'IDEA_FullSilicon']
 detectors = [
     'IDEA',
+    'IDEA_updated',
     'IDEA_3T',
     'IDEA_FullSilicon',
     'IDEA_SiTracking',
